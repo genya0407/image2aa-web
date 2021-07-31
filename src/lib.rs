5c692610358368d936269a29e72cd3b6f178f59f
Yusuke Sangenya <longinus.eva@gmail.com>
2021-07-31 17:18:12 +0900

    tmp

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,28 @@ extern crate image;
 extern crate imageproc;
 extern crate rusttype;
 
-use imageproc::drawing::draw_text_mut;
 use image::{Rgb, RgbImage};
-use image::ImageBuffer;
-use rusttype::{FontCollection, Scale};
+use imageproc::drawing::draw_text_mut;
+use rusttype::Font;
+use rusttype::Scale;
 
 pub fn text2image(text: String) -> image::RgbImage {
     let font = Vec::from(include_bytes!("../assets/font/VL-Gothic-Regular.ttf") as &[u8]);
-    let font = FontCollection::from_bytes(font).unwrap().into_font().unwrap();
+    let font = Font::try_from_vec(font).unwrap();
 
     let x_font_size = 5f32;
     let y_font_size = 10f32;
-    let scale = Scale { x: x_font_size * 2., y: y_font_size };
+    let scale = Scale {
+        x: x_font_size * 2.,
+        y: y_font_size,
+    };
 
     let longest_line_length = text.lines().map(font_ascii_count).max().unwrap() / 2;
     let line_count = text.lines().count();
 
     let image_width = ((x_font_size * 2.) * longest_line_length as f32) as u32;
     let image_height = (line_count as f32 * y_font_size) as u32;
-    let mut image = RgbImage::from_pixel(image_width, image_height, Rgb([255,255,255]));
+    let mut image = RgbImage::from_pixel(image_width, image_height, Rgb([255, 255, 255]));
 
     for (v_index, line) in text.lines().enumerate() {
         let mut h_index = 0u32;
@@ -28,8 +31,11 @@ pub fn text2image(text: String) -> image::RgbImage {
             draw_text_mut(
                 &mut image,
                 Rgb([0u8, 0u8, 0u8]),
-                (h_index as f32 * x_font_size) as u32, (v_index as f32 * y_font_size) as u32,
-                scale, &font, &c.to_string()
+                (h_index as f32 * x_font_size) as u32,
+                (v_index as f32 * y_font_size) as u32,
+                scale,
+                &font,
+                &c.to_string(),
             );
 
             if c.is_ascii() {
@@ -44,11 +50,7 @@ pub fn text2image(text: String) -> image::RgbImage {
 }
 
 fn font_ascii_count(text: &str) -> u32 {
-    text.chars().map(|char| {
-        if char.is_ascii() {
-            1
-        } else {
-            2
-        }
-    }).sum::<u32>()
+    text.chars()
+        .map(|char| if char.is_ascii() { 1 } else { 2 })
+        .sum::<u32>()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,46 @@
 #![feature(plugin)]
-#![feature(custom_derive)]
-#![plugin(rocket_codegen)]
+#![feature(proc_macro_derive)]
 
 extern crate dotenv;
+#[macro_use]
 extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
+extern crate image;
 extern crate image2aa;
 extern crate image2aa_web;
-extern crate rusoto_core;
-extern crate rusoto_credential;
-extern crate rusoto_s3;
-extern crate image;
-extern crate time;
-extern crate sha3;
 extern crate rand;
+extern crate sha3;
+extern crate time;
 
-use std::io;
-use std::io::Read;
+use image2aa::{filter, utils};
+use rand::Rng;
+use rocket::http::Status;
+use rocket::request::LenientForm;
+use rocket::response::{NamedFile, Responder};
+use rocket::tokio::io::AsyncReadExt;
+use rocket::{Request, Response, State};
+use rocket_contrib::Json;
+use sha3::Sha3_256;
 use std::env;
+use std::io;
+use std::path::Path;
 use std::sync::mpsc;
 use std::sync::mpsc::SyncSender;
 use std::thread;
-use std::path::Path;
-use rocket::response::{Responder, NamedFile};
-use rocket::request::LenientForm;
-use rocket::{Request, Response, State};
-use rocket::http::Status;
-use rocket_contrib::Json;
-use rusoto_s3::S3;
-use sha3::{Digest, Sha3_256};
-use image2aa::{filter, utils};
-use rand::Rng;
 
 struct ContentDisposition(NamedFile, String);
 
-impl<'r> Responder<'r> for ContentDisposition {
+impl<'r, 'o: 'r> Responder<'r, 'o> for ContentDisposition {
     fn respond_to(self, request: &Request) -> Result<Response<'r>, Status> {
         let filename = self.1.clone();
         match self.0.respond_to(request) {
             Ok(mut response) => {
                 response.adjoin_raw_header(
                     "Content-Disposition",
-                    format!("attachment; filename=\"{}\"", filename)
+                    format!("attachment; filename=\"{}\"", filename),
                 );
                 Ok(response)
-            },
-            Err(status) => Err(status)
+            }
+            Err(status) => Err(status),
         }
     }
 }
@@ -54,50 +49,16 @@ impl<'r> Responder<'r> for ContentDisposition {
 struct Options {
     blocksize: Option<usize>,
     char_detect_thresh: Option<u32>,
-    line_detect_thresh: Option<u32>
+    line_detect_thresh: Option<u32>,
 }
 
 #[derive(FromForm)]
 struct AsciiArtForm {
-    text: String
+    text: String,
 }
 
 struct S3Uploader {
-    uploaded_image_hashes: Vec<String>
-}
-
-impl S3Uploader {
-    pub fn new() -> Self {
-        Self { uploaded_image_hashes: vec![] }
-    }
-
-    pub fn upload_s3(&mut self, image_buf: Vec<u8>) {
-        let mut hasher = Sha3_256::default();
-        hasher.input(&image_buf);
-        let image_hash = format!("{:x}", hasher.result());
-
-        if !self.uploaded_image_hashes.contains(&image_hash) {
-            self.uploaded_image_hashes.push(image_hash.clone());
-
-            let file_extension = match image::guess_format(&image_buf) {
-                Ok(image::ImageFormat::PNG) => "png",
-                Ok(image::ImageFormat::JPEG) => "jpg",
-                _ => "unknown"
-            };
-
-            let s3 = rusoto_s3::S3Client::new(
-                rusoto_core::reactor::RequestDispatcher::default(),
-                rusoto_credential::EnvironmentProvider,
-                rusoto_core::Region::ApNortheast1
-            );
-            let bucket_name = env::var("AWS_S3_BUCKET_NAME").unwrap();
-            let mut put_request = rusoto_s3::PutObjectRequest::default();
-            put_request.bucket = bucket_name;
-            put_request.key = format!("{}_{}.{}", time::now().to_timespec().sec, image_hash, file_extension);
-            put_request.body = Some(image_buf);
-            s3.put_object(&put_request).sync().unwrap();
-        }
-    }
+    uploaded_image_hashes: Vec<String>,
 }
 
 #[get("/")]
@@ -111,7 +72,10 @@ fn download_aa_image(ascii_art: LenientForm<AsciiArtForm>) -> io::Result<Content
     let filename = format!(
         "/tmp/{}_{}.png",
         time::now().to_timespec().sec,
-        rand::thread_rng().gen_ascii_chars().take(20).collect::<String>()
+        rand::thread_rng()
+            .gen_ascii_chars()
+            .take(20)
+            .collect::<String>()
     );
     let path = Path::new(&filename);
     let image = image2aa_web::text2image(ascii_art.get().text.clone());
@@ -119,32 +83,42 @@ fn download_aa_image(ascii_art: LenientForm<AsciiArtForm>) -> io::Result<Content
     NamedFile::open(&filename).map(|named_file| {
         ContentDisposition(
             named_file,
-            path.file_name().unwrap().to_string_lossy().to_string()
+            path.file_name().unwrap().to_string_lossy().to_string(),
         )
     })
 }
 
 #[post("/image", data = "<image_binary>")]
-fn image_without_options(image_binary: rocket::Data, tx: State<SyncSender<Vec<u8>>>) -> Json {
-    let options = Options { blocksize: None, char_detect_thresh: None, line_detect_thresh: None };
-    image(options, image_binary, tx)
+fn image_without_options(image_binary: rocket::Data) -> Json {
+    let options = Options {
+        blocksize: None,
+        char_detect_thresh: None,
+        line_detect_thresh: None,
+    };
+    image(options, image_binary)
 }
 
 #[post("/image?<options>", data = "<image_binary>")]
-fn image(options: Options, image_binary: rocket::Data, tx: State<SyncSender<Vec<u8>>>) -> Json {
+fn image(options: Options, image_binary: rocket::Data) -> Json {
     let mut image_buf = vec![];
-    image_binary.open().read_to_end(&mut image_buf).expect("Failed to read image.");
-
-    tx.send(image_buf.clone()).expect("Failed to upload image.");
+    image_binary.open().read_to_end(&mut image_buf).await;
 
     let mut hough_filter = filter::hough::default();
-    if let Some(block_size) = options.blocksize { hough_filter.block_size = block_size; }
-    if let Some(slope_count_thresh) = options.char_detect_thresh { hough_filter.slope_count_thresh = slope_count_thresh; }
+    if let Some(block_size) = options.blocksize {
+        hough_filter.block_size = block_size;
+    }
+    if let Some(slope_count_thresh) = options.char_detect_thresh {
+        hough_filter.slope_count_thresh = slope_count_thresh;
+    }
 
     let mut binary_filter = filter::binary::default();
-    if let Some(thresh) = options.line_detect_thresh { binary_filter.thresh = thresh; }
+    if let Some(thresh) = options.line_detect_thresh {
+        binary_filter.thresh = thresh;
+    }
 
-    let image_array = utils::read_image(image_buf.as_slice()).map_err(|e| println!("{}", e)).unwrap();
+    let image_array = utils::read_image(image_buf.as_slice())
+        .map_err(|e| println!("{}", e))
+        .unwrap();
 
     let grayscale_array = filter::grayscale::default().run(image_array);
     let gradient_array = filter::line::default().run(grayscale_array.clone());
@@ -157,18 +131,10 @@ fn image(options: Options, image_binary: rocket::Data, tx: State<SyncSender<Vec<
 fn main() {
     dotenv::dotenv().ok();
 
-    let (tx, rx) = mpsc::sync_channel(10);
-
-    thread::spawn(move || {
-        let mut uploader = S3Uploader::new();
-        loop {
-            let image_buf = rx.recv().expect("Failed to receive image.");
-            uploader.upload_s3(image_buf);
-        }
-    });
-
     rocket::ignite()
-        .manage(tx)
-        .mount("/", routes![index, image, image_without_options, download_aa_image])
+        .mount(
+            "/",
+            routes![index, image, image_without_options, download_aa_image],
+        )
         .launch();
 }
